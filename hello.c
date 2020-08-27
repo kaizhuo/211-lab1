efc072f74b3336dbe6a719745632e77395bee711
kaizhuo <kaizhuo@ad.unc.edu>
2020-08-27 19:34:40 +0000

    First Commit!

---
+++ hello.c
@@ -0,0 +1,12 @@
+// 730444890
+// I pledge the COMP211 honor code.
+ #include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("hello,world\n");
+    printf("welcome to c!\n");
+    return EXIT_SUCCESS;
+}
+
+
